cd800f4fc41ec211060431ab6ef99c09b4b9032f
diqiuxin <501993589@qq.com>
2017-11-17 18:12:45 +0800

    server.js的修正

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # NodeJS-Server-rep
+
+## 仓库存储信息
+脚本数量：1
+
+脚本名：server.js
+
+## 脚本文件详细说明 
+
+# server.js
+
+>server.js 用于http请求时的文件发送
+
+#### 脚本说明(Script explanation):
+    
+**1. 运行方式：**
+
+    ```
+    $node server.js port
+    ```
+      server.js  --脚本文件(可以不加后缀)
+      port       --端口(1023<port<65536)
+
+  >在浏览器中输入: localhost:port
+
+**2. 运行过程路径提示：** 'HTTP 路径为' 、 'xxx文件已经发送'
+    
+**4. 报错提示:**  '客户端有点小错误，404已经发送 :) ' 
+  
+**5. 脚本功能：** 通过判断HTTP请求时的路径，来发送指定文件到客户端，并在后台打印文件发送状态
+          
+
--- server.js
+++ server.js
@@ -1,50 +1,58 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var port = process.argv[2];
 
 if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
+  console.log('请指定端口号 eg: \nnode server.js 8888 ');
+  process.exit(1);
 }
 
 var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
-  var method = request.method
+  var parsedUrl = url.parse(request.url, true);
+  var path = request.url;
+  var query = '';
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')); }
+  var pathNoQuery = parsedUrl.pathname;
+  var queryObject = parsedUrl.query;
+  var method = request.method;
 
   /******** 从这里开始看，上面不要看 ************/
+console.log('-----------------------');
+console.log('HTTP 路径为: \n' + path);
 
-console.log('HTTP 路径为\n' + path);
-if(path == '/style.js'){
-  response.setHeader('Conten-Type', 'text/css; charset=utf-8');
-  response.write('body{background-color: #ddd;}h1{color: red;}');
-  response.end();
-}else if(path == '/script.html'){
-  response.setHeader('Conten-Type', 'text/javascript; charset=utf-8');
-  response.write('alert("这是JS执行的 :) ")');
-  response.end();
-}else if(path == '/index.css'){
-  response.setHeader('Conten-Type', 'text/html; charset=utf-8');
-  response.write('<!DOCTYPE>\n<html>'  + 
-  '<head><link rel="stylesheet" href="/style.js">' +
+if(path == '/' ){
+  response.setHeader('Content-Type', 'text/html; charset=utf-8');
+  response.write('<!DOCTYPE>\n' +
+  '<html><head>' + 
+  '<link rel="stylesheet" href="/style.css">' +
   '</head><body>'  +
   '<h1>你好</h1>' +
-  '<script src="/script.html"></script>' +
+  '<h3> <a href="http://www.diqiuxin.top">地球心-赵博-杭州</a> </h3>' +
+  '<script src="/main.js"></script>' +
   '</body></html>');
   response.end();
-}else{
+  console.log('html文件已经发送');
+}else if(path == '/style.css'){
+  response.setHeader('Content-Type', 'text/css; charset=utf-8');
+  response.write('body{background-color: #ddd;}h1{color: red;}');
+  response.end();
+  console.log('css文件已经发送');
+}else if(path == '/main.js'){
+  response.setHeader('Content-Type', 'text/javascript; charset=utf-8');
+  response.write('alert("这是JS执行的 :) ")');
+  response.end();
+  console.log('js文件已经发送');
+}else {
   response.statusCode = 404;
   response.end();
+  console.log('客户端有点小错误，404状态 已经发送 :) ');
 }
 
   /******** 代码结束，下面不要看 ************/
-})
+});
 
-server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+server.listen(port);
+console.log('正在监听 ' + port + ' 中... \n ' +
+  '可用浏览器打开 http://localhost:' + port);
 
